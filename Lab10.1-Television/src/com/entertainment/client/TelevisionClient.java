7f78bb97c33f90768977f11f8390636ab8ebb20f
Jay Rostosky <jay.rostosky@tlgcohort.com>
2023-10-26 09:39:56 -0700

    Lab10.1-Television complete

---
+++ Lab10.1-Television/src/com/entertainment/client/TelevisionClient.java
@@ -0,0 +1,52 @@
+package com.entertainment.client;
+
+import com.entertainment.DisplayType;
+import com.entertainment.InvalidBrandException;
+import com.entertainment.Television;
+
+import static com.entertainment.DisplayType.*;
+
+/*
+ * Application main-class, i.e., the class definition with the main() method.
+ * Purpose: create a few instances of Television and give them a test-drive.
+ */
+class TelevisionClient {
+
+    // application starting point - at runtime, the app starts here
+    public static void main(String[] args) throws InvalidBrandException {
+        System.out.println(Television.getInstanceCount() + " instances have been created");
+        System.out.println();
+
+        // create an instance of Television and set its properties via setter calls
+        Television tv1 = new Television();
+        tv1.setBrand("Magnavox");
+        tv1.setVolume(32);
+        tv1.setDisplay(PLASMA);
+
+        // create a 2nd instance of Television via the brand-volume-display ctor
+        Television tv2 = new Television("Sony", 5, OLED);
+
+        // create a third Television object by specifying only the brand in the ctor call
+        Television tv3 = new Television("LG");
+
+        // turn them all on
+        tv1.turnOn();
+        tv2.turnOn();
+        tv3.turnOn();
+        System.out.println();  // blank line in the output
+
+        // turn them all off
+        tv1.turnOff();
+        tv2.turnOff();
+        tv3.turnOff();
+        System.out.println();
+
+        // show their toString() methods in action
+        System.out.println(tv1.toString());
+        System.out.println(tv2.toString());
+        System.out.println(tv3);  // toString() automatically called
+        System.out.println();
+
+        System.out.println(Television.getInstanceCount() + " instances have been created");
+    }
+}
