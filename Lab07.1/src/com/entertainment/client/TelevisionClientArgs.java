2bab319e81d0ce4559aeb27954748077f63877d9
Jay Rostosky <jay.rostosky@tlgcohort.com>
2023-10-26 11:55:25 -0700

    Minor adjustments

--- Lab07.1/src/com/entertainment/client/TelevisionClientArgs.java
+++ Lab07.1/src/com/entertainment/client/TelevisionClientArgs.java
@@ -1,4 +1,6 @@
-package com.entertainment.client;/*
+package com.entertainment.client;
+
+/*
  * This code is sample code, provided as-is, and we make no
  * warranties as to its correctness or suitability for any purpose.
  *
