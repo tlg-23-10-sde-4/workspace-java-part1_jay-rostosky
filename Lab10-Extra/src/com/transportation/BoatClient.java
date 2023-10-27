5d539ffc7659d70bf1fa4c7df7d77f38728df033
Jay Rostosky <jay.rostosky@tlgcohort.com>
2023-10-27 12:40:37 -0700

    Boat.moveTo() no exception thrown

---
+++ Lab10-Extra/src/com/transportation/BoatClient.java
@@ -0,0 +1,9 @@
+package com.transportation;
+
+class BoatClient {
+
+    public static void main(String[] args) {
+        Boat boat = new Boat("ABCDE90");
+        boat.moveTo("West Seattle");
+    }
+}
