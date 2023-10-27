e7bef69b6f7fa002b34cb305f953cb006e1d6521
Jay Rostosky <jay.rostosky@tlgcohort.com>
2023-10-27 09:16:53 -0700

    Shape iteration 1 (Circle, Rectangle)

--- Shape/src/edu/geometry/client/ShapeClient.java
+++ Shape/src/edu/geometry/client/ShapeClient.java
@@ -1,8 +1,23 @@
 package edu.geometry.client;
 
+import edu.geometry.Circle;
+import edu.geometry.Rectangle;
+import edu.geometry.Shape;
+
 class ShapeClient {
 
     public static void main(String[] args) {
+        Shape[] shapes = new Shape[4];
 
+        shapes[0] = new Circle(2.5);  // 19.63
+        shapes[1] = new Circle(3.2);  // 32.16
+
+        shapes[2] = new Rectangle(2.0, 3.0);  // 6.0
+        shapes[3] = new Rectangle(2.0, 4.0);  // 8.0
+
+        for (Shape shape : shapes) {
+            // desire: The area of the Circle is 4.56
+            System.out.printf("The area of the %s is %s\n", shape.getClass().getSimpleName(), shape.area());
+        }
     }
 }
